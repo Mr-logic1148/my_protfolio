8430a29bda24e0744e0dd17f040d78f599793001
Mr-logic1148 <mehrajgaud1148@gmail.com>
2026-05-01 11:54:42 +0100

    refactor Skills and Timeline components for consistent spacing in headings

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -101,7 +101,7 @@ export default function Skills() {
 
         <h2 className="mt-6 text-3xl font-semibold text-main md:text-5xl">
           Skills that balance
-          <span className="pb-3 block bg-gradient-to-r from-indigo-300 via-violet-300 to-fuchsia-300 bg-clip-text text-transparent">
+          <span className="block pb-3 bg-gradient-to-r from-indigo-300 via-violet-300 to-fuchsia-300 bg-clip-text text-transparent">
             engineering, data, and delivery
           </span>
         </h2>
